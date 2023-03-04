b867083dea6859f99827efa035bc43090b45edb2
IrynaBuchma <buchma.iryna2018@gmail.com>
2023-03-04 10:45:08 +0200

    add babel config json

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,54 @@
+const SimpleLightbox = require('simple-lightbox');
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+
+const gallery = document.querySelector('.gallery'); 
+
+gallery.addEventListener('click', onImageClick);
+
+const markedGallery = markupGallery(galleryItems);
+
+gallery.innerHTML = markedGallery; //
+
+
+
+function markupGallery(items) {
+    return items.map(({ preview, original, description }) => { 
+        return ` 
+            <div class="gallery__item"> 
+                <a class="gallery__link" href="${original}">
+                    <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/>
+                </a>
+            </div>`;
+    }).join(''); 
+};
+
+function onImageClick(e) {
+    if (!e.target.classList.contains("gallery__image")) {
+        return;
+    }
+    e.preventDefault();
+    
+    
+    const instance = basicLightbox.create(`
+    <img src="${e.target.dataset.source}" width="" height="">
+`)
+    instance.show();
+    document.addEventListener("keydown", onEscapePress);
+    document.body.classList.add(".js-open-modal");
+
+function onEscapePress(e) {
+    if (e.code === "Escape" && basicLightbox.visible()) {
+    closeModal();
+  }
+};
+
+function closeModal() {
+    document.removeEventListener("keydown", onEscapePress);
+    document.body.classList.remove(".js-close-modal");
+    instance.close();
+}
+};
 
 console.log(galleryItems);
